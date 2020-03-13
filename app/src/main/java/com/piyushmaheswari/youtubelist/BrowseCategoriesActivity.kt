a507f0c6a61481f186f970a5b1b0ba2655f8f67e
piyushmaheswari <piyush.ext@grofers.com>
2020-03-13 22:15:09 +0530

    Initial commit

---
+++ app/src/main/java/com/piyushmaheswari/youtubelist/BrowseCategoriesActivity.kt
@@ -0,0 +1,12 @@
+package com.piyushmaheswari.youtubelist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BrowseCategoriesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_browse_categories)
+    }
+}
